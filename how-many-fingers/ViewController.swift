1f919192ffa9e94dfe8d16ca8a706eae791d29bc
PronovaDesigns <migliorini.christopher@gmail.com>
2016-08-03 20:00:47 -0400

    Initial Commit

---
+++ how-many-fingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  how-many-fingers
+//
+//  Created by Christopher Migliorini on 8/3/16.
+//  Copyright © 2016 ObviPop LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
